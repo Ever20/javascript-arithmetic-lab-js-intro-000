1d04d2e7730d8e58fcbb477b2eaeda8ec976a819
Francisco trejo <f85.trejo@gmail.com>
2021-03-01 01:58:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,22 @@ function divide() {
   return '/'
 }
 
-function addNumbers(a, b) {
-  var     = a+b;
-return  '1511'
+function add(a,b) {
+  return a + b
+}
 
+function subtract(a,b) {
+  return a - b
+}
+
+function divide(a,b) {
+  return a / b
+}
+
+function multiply(a,b) {
+  return a * b
+}
+
+function increment(n) {
+  return n += 1
 }
