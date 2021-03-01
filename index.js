0c6d3c098e2bcd5569177d11deb48e573cc12794
Francisco trejo <f85.trejo@gmail.com>
2021-03-01 02:08:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,3 +33,11 @@ function multiply(a,b) {
 function increment(n) {
   return n += 1
 }
+
+function decrement(n) {
+  return n -= 1
+}
+
+function makeInt(string) {
+  return parseInt(string, 10)
+}
